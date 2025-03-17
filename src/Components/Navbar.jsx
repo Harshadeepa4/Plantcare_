668fa97031cdb87dc0512e44a5b33513bd1669d3
Harshadeepa4 <155470703+Harshadeepa4@users.noreply.github.com>
2025-03-17 21:44:50 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,6 +6,7 @@ import { Menu, X } from "lucide-react";
 export const Navbar = () => {
   const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
   const [activeButton, setActiveButton] = useState(null);
+  const [signInActive, setSignInActive] = useState(false); 
   const toggleNavbar = () => {
     setMobileDrawerOpen(!mobileDrawerOpen);
   };
@@ -48,13 +49,24 @@ export const Navbar = () => {
     }
   };
 
+  const handleSignInClick = () => {
+    setSignInActive(true);
+    
+    window.location.href = "https://b7fa-2409-40f2-304b-fd9b-15fb-f415-dca1-b42a.ngrok-free.app/";
+  };
+
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto relative text-sm">
         <div className="flex justify-between items-center">
           <div className="flex item-center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
-            <span className="text-x1 tracking-tight" style={{ fontSize: '1.75rem', lineHeight: '2.50rem' }}>Plant Care</span>
+            <span
+              className="text-x1 tracking-tight"
+              style={{ fontSize: "1.75rem", lineHeight: "2.50rem" }}
+            >
+              Plant Care
+            </span>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
@@ -85,12 +97,14 @@ export const Navbar = () => {
             </li>
           </ul>
           <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <a
-              href="#"
-              className="bg-gradient-to-r from-orange-500 to-orange-900 py-2 px-3 border rounded-md"
+            <button
+              onClick={handleSignInClick}
+              className={`bg-gradient-to-r from-orange-500 to-orange-900 py-2 px-3 border rounded-md ${
+                signInActive ? "active" : ""
+              }`}
             >
               Sign In
-            </a>
+            </button>
             <button
               onClick={handleUserProfileClick}
               className={`bg-gradient-to-r from-orange-500 to-orange-900 py-2 px-3 rounded-md ${
@@ -137,13 +151,12 @@ export const Navbar = () => {
               </li>
             </ul>
             <div className="flex space-x-6">
-              <a
-                href=""
-                className="py-2 px-3 bg-gradient-to-r from-orange-500 to-orange-900  rounded-md"
+              <button
+                onClick={handleSignInClick}
+                className="py-2 px-3 bg-gradient-to-r from-orange-500 to-orange-900 rounded-md"
               >
                 Sign In
-              </a>
-            
+              </button>
             </div>
           </div>
         )}
